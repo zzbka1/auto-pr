a100913712b6b1118c5e78e2d76bc51f3fcd80cb
zzbka1 <shuai.cao@autodesk.com>
2024-04-17 15:11:31 +0800

    init

--- com/cs/controller/LoginController.java
+++ com/cs/controller/LoginController.java
@@ -3,5 +3,8 @@ package com.cs.controller;
 public class LoginController {
 
     // This method is used to login the user
-
+    public void login() {
+        // this is a dummy method
+        System.out.println("User logged in successfully");
+    }
 }
